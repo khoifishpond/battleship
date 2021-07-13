fba531d1e7df6a53ff8f3139db64b9bd927ffa0c
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-13 12:06:48 -0700

    Finish blueprint of spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,13 +1,41 @@
 require 'rspec'
 require './lib/board'
+require './lib/ship'
 require './lib/cell'
 
 describe Board do
   before(:each) do
     board = Board.new
+    cruiser = Ship.new('Cruiser', 3)
+    submarine = Ship.new('Submarine', 2)
   end
 
   it 'exists' do
+    # TODO: expectation here
+  end
 
+  # will it return a hash of coordinates?
+  xit '' do
+    # TODO: expectation here
+  end
+
+  # is this coordinate or cell valid?
+  xit '' do
+    # TODO: expectation here
+  end
+
+  # is the ship's placement valid?
+  xit '' do
+    # TODO: expectation here
+  end
+
+  # when placed, are the coordinates consecutive?
+  xit '' do
+    # TODO: expectation here
+  end
+
+  # ships cannot be placed diagonally
+  xit '' do
+    # TODO: expectation here
   end
 end
