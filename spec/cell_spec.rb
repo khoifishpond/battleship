d2bb295114e24a46dbe701034bc8e58e2601a687
Kelsey Thompson <kelsthompson2@gmail.com>
2021-07-12 22:30:25 -0400

    Add render test

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -52,4 +52,21 @@ describe "Cell" do
       expect(@cell.ship.health).to eq(2)
     end
   end
+
+  context "Render" do
+    before(:each) do
+      @cell_1 = Cell.new("B4")
+      @cell_2 = Cell.new("C3")
+      @cruiser = Ship.new("Cruiser", 3)
+      @cell_2.place_ship(@cruiser)
+    end
+
+    it "can render" do
+      expect(@cell_1.render).to eq('.')
+      expect(@cell_2.render).to eq('.')
+      expect(@cell_2.render(true)).to eq('S')
+
+
+    end
+  end
 end
