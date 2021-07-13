4295941f39888a7f8b6d760a8dfd842c6dfc3f0d
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-12 17:38:40 -0700

    Fill in tests

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -3,46 +3,47 @@ require './lib/ship'
 
 describe Ship do
   context 'Attributes' do
-    # before each test, run this
     before(:each) do
-      # creates a new Ship object
       @cruiser = Ship.new("Cruiser", 3)
     end
 
     it 'exists' do
-      # Kelsey writes a method to satisfy this test
       expect(@cruiser).to be_a(Ship)
     end
 
-    xit 'has a name' do
-      # Kelsey writes a method to satisfy this test
+    it 'has a name' do
       expect(@cruiser.name).to eq("Cruiser")
     end
 
-    xit 'has a length' do
-      # Kelsey writes a method and expectation to satisfy this test
-      expect().to eq()
+    it 'has a length' do
+      expect(@cruiser.length).to eq(3)
     end
 
-    xit 'has health' do
-      # Kelsey writes a method and expectation to satisfy this test
-      expect().to eq()
+    it 'has health' do
+      expect(@cruiser.health).to eq(3)
     end
 
-    xit 'has a sunk status' do
-      # Kelsey writes both the test and method
+    it 'has a sunk status' do
+      expect(@cruiser.sunk?).to eq(false)
     end
 
-    xit 'can be hit' do
-      # Kelsey writes both the test and method
+    it 'can be hit and will lose one health' do
+      expect(@cruiser.health).to eq(3)
+
+      @cruiser.hit
+
+      expect(@cruiser.health).to eq(2)
     end
 
-    xit 'will lose health if hit' do
-      # Kelsey writes both the test and method
-    end
+    it 'will sink at 0 health' do
+      expect(@cruiser.sunk?).to eq(false)
 
-    xit 'will sink at 0 health' do
-      # Kelsey writes both the test and method
+      @cruiser.hit
+      @cruiser.hit
+      @cruiser.hit
+
+      expect(@cruiser.health).to eq(0)
+      expect(@cruiser.sunk?).to eq(true)
     end
   end
 end
