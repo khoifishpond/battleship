9e26246c6124d66f9b08a09562bb99433b5a8be2
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-13 17:50:50 -0700

    Add valid coordinates to spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,29 +15,17 @@ describe Board do
   end
 
   it 'has cells' do
-    expect(@board.cells).to eq({
-      "A1" => Cell.new("A1"),
-      "A2" => Cell.new("A2"),
-      "A3" => Cell.new("A3"),
-      "A4" => Cell.new("A4"),
-      "B1" => Cell.new("B1"),
-      "B2" => Cell.new("B2"),
-      "B3" => Cell.new("B3"),
-      "B4" => Cell.new("B4"),
-      "C1" => Cell.new("C1"),
-      "C2" => Cell.new("C2"),
-      "C3" => Cell.new("C3"),
-      "C4" => Cell.new("C4"),
-      "D1" => Cell.new("D1"),
-      "D2" => Cell.new("D2"),
-      "D3" => Cell.new("D3"),
-      "D4" => Cell.new("D4")
-    })
+    expect(@board.cells).to be_a(Hash)
+    expect(@board.cells["A1"]).to be_a(Cell)
+    expect(@board.cells["D3"]).to be_a(Cell)
   end
 
-  # is this coordinate or cell valid?
-  xit '' do
-    # TODO: expectation here
+  it 'has valid coordinates' do
+    expect(@board.valid_coordinate?("A1")).to eq(true)
+    expect(@board.valid_coordinate?("D4")).to eq(true)
+    expect(@board.valid_coordinate?("A5")).to eq(false)
+    expect(@board.valid_coordinate?("E1")).to eq(false)
+    expect(@board.valid_coordinate?("A22")).to eq(false)
   end
 
   # is the ship's placement valid?
