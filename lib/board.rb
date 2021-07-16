0037ddce7ab06f230d42d11518bca32eb7d49855
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-15 20:50:33 -0700

    Update keys to symbols

--- lib/board.rb
+++ lib/board.rb
@@ -1,36 +1,30 @@
-require 'pry'
 class Board
   attr_reader :cells
 
   def initialize
     # rename these variables? method has the same name
-    @cells = cells
-  end
-
-  def cells
-    # we can use ordinal values here to create a board?
     @cells = {
-      "A1" => Cell.new("A1"),
-      "A2" => Cell.new("A2"),
-      "A3" => Cell.new("A3"),
-      "A4" => Cell.new("A4"),
-      "B1" => Cell.new("B1"),
-      "B2" => Cell.new("B2"),
-      "B3" => Cell.new("B3"),
-      "B4" => Cell.new("B4"),
-      "C1" => Cell.new("C1"),
-      "C2" => Cell.new("C2"),
-      "C3" => Cell.new("C3"),
-      "C4" => Cell.new("C4"),
-      "D1" => Cell.new("D1"),
-      "D2" => Cell.new("D2"),
-      "D3" => Cell.new("D3"),
-      "D4" => Cell.new("D4")
+      :A1 => Cell.new("A1"),
+      :A2 => Cell.new("A2"),
+      :A3 => Cell.new("A3"),
+      :A4 => Cell.new("A4"),
+      :B1 => Cell.new("B1"),
+      :B2 => Cell.new("B2"),
+      :B3 => Cell.new("B3"),
+      :B4 => Cell.new("B4"),
+      :C1 => Cell.new("C1"),
+      :C2 => Cell.new("C2"),
+      :C3 => Cell.new("C3"),
+      :C4 => Cell.new("C4"),
+      :D1 => Cell.new("D1"),
+      :D2 => Cell.new("D2"),
+      :D3 => Cell.new("D3"),
+      :D4 => Cell.new("D4")
     }
   end
 
   def valid_coordinate?(coordinate)
-    @cells.key?(coordinate)
+    @cells.key?(coordinate.to_sym)
   end
 
   def fits?(ship, coordinates)
@@ -81,7 +75,7 @@ class Board
 
   def not_overlapping?(coordinates)
     coordinates.all? do |coordinate|
-      @cells[coordinate].is_empty
+      @cells[coordinate.to_sym].is_empty
     end
   end
 
@@ -100,7 +94,7 @@ class Board
   def place(ship, coordinates)
     if valid_placement?(ship, coordinates)
       coordinates.each do |coordinate|
-        @cells[coordinate].place_ship(ship)
+        @cells[coordinate.to_sym].place_ship(ship)
       end
     end
   end
@@ -108,17 +102,17 @@ class Board
   def render(reveal = false)
     rendered_board =
     "  1 2 3 4 \n" +
-    "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render} \n" +
-    "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render} \n" +
-    "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render} \n" +
-    "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render} \n"
+    "A #{@cells[:A1].render} #{@cells[:A2].render} #{@cells[:A3].render} #{@cells[:A4].render} \n" +
+    "B #{@cells[:B1].render} #{@cells[:B2].render} #{@cells[:B3].render} #{@cells[:B4].render} \n" +
+    "C #{@cells[:C1].render} #{@cells[:C2].render} #{@cells[:C3].render} #{@cells[:C4].render} \n" +
+    "D #{@cells[:D1].render} #{@cells[:D2].render} #{@cells[:D3].render} #{@cells[:D4].render} \n"
 
     rendered_board_revealed =
     "  1 2 3 4 \n" +
-    "A #{@cells["A1"].render(true)} #{@cells["A2"].render(true)} #{@cells["A3"].render(true)} #{@cells["A4"].render(true)} \n" +
-    "B #{@cells["B1"].render(true)} #{@cells["B2"].render(true)} #{@cells["B3"].render(true)} #{@cells["B4"].render(true)} \n" +
-    "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)} \n" +
-    "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)} \n"
+    "A #{@cells[:A1].render(true)} #{@cells[:A2].render(true)} #{@cells[:A3].render(true)} #{@cells[:A4].render(true)} \n" +
+    "B #{@cells[:B1].render(true)} #{@cells[:B2].render(true)} #{@cells[:B3].render(true)} #{@cells[:B4].render(true)} \n" +
+    "C #{@cells[:C1].render(true)} #{@cells[:C2].render(true)} #{@cells[:C3].render(true)} #{@cells[:C4].render(true)} \n" +
+    "D #{@cells[:D1].render(true)} #{@cells[:D2].render(true)} #{@cells[:D3].render(true)} #{@cells[:D4].render(true)} \n"
 
     if reveal == true
       rendered_board_revealed
