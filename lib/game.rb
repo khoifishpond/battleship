e1b7982c52baca7447d3a30152c3bfc109a18a53
Kelsey Thompson <kelsthompson2@gmail.com>
2021-07-17 18:06:55 -0400

    Move player and computer methods to respective class

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,13 @@
 class Game
-  attr_reader :welcome_message, :board
+  attr_reader :welcome_message, :board, :computer, :player
 
-  def initialize(board)
+  def initialize
     @welcome_message = "Welcome to BATTLESHIP\nEnter p to play. Enter q to quit."
-    @board = board
+    @computer_board = Board.new
+    @player_board = Board.new
+    # @player = player
+    # @computer = computer
+    # require "pry";binding.pry
   end
 
   def main_menu
@@ -11,29 +15,14 @@ class Game
   end
 
   def play
-    
+
   end
 
   def quit
     "GG LOSER"
   end
 
-  def ship_placement(ship)
-    
-    computer_coords = @board.cells.keys.sample(ship.length)
-    
-    until true
-      until valid_placement?(ship, computer_coords)
-        @board.place(ship, computer_coords)
-      end
-    end
 
-  end
 
-  def computer_fire_at(cell)
-    target_practice = @board.cells.keys
-    incel = target_practice.sample
-    target_practice.delete(incel)
-    incel[cell.to_sym].fire_upon
-  end
+
 end
