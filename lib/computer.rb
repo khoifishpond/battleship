5c075074e22d92332508eeff7ced6ca63e0c640d
Kelsey Thompson <kelsthompson2@gmail.com>
2021-07-17 17:16:37 -0400

    Complete random selection method for computer

--- lib/computer.rb
+++ lib/computer.rb
@@ -2,10 +2,24 @@ require './lib/player'
 
 class Computer < Player
 
-  def random_selection
+  def random_selection(ship)
+    all_positions_possible = []
+    rows_columns = ["A", "B", "C", "D", "1", "2", "3", "4"]
+    rows_columns.each do |line|
+      @board.cells.keys.select do |key|
+        if key[0] == line || key[1] == line
+          all_positions_possible << key
+        end
+      end
+    end
 
+    new_array = all_positions_possible.each_cons(ship.length).to_a
 
-    # require "pry";binding.pry
+    final_array = new_array.find_all do |keys|
+        @board.consecutive?(keys)
+    end
+
+    final_array.sample
   end
 
 end
