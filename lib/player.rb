ecdfcf7af6b0bde3f6f934d7b077bf7360eb7372
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-18 20:10:51 -0700

    Move instance variable to initialize

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,10 @@
 class Player
-  attr_reader :coordinates, :player_board, :computer_board
+  attr_reader :all_player_cells, :player_board, :computer_board
 
   def initialize(player_board, computer_board)
     @player_board = player_board
     @computer_board = computer_board
+    @all_player_cells = @player_board.cells.keys
   end
 
   # def select_coordinates
